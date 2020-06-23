f9f4f49b1e76d3e962c803a5e2351325a4f9cdf1
SunOne <44489132+SunOner@users.noreply.github.com>
2020-06-23 20:12:08 +0300

    Main update

--- WarfaceAuth/Program.cs
+++ WarfaceAuth/Program.cs
@@ -14,30 +14,57 @@ namespace WarfaceAuth
         static void Main(string[] args)
         {
             Auth Auth = new Auth();
+            string help_ = "example:\nWarfaceAuth <login> <password> <ru-alpha|ru-bravo|ru-charlie|ru-delta> <g> <F:/mailru/Warface/Bin64Release/Game.exe> (for game) or \nWarfaceAuth <login> <password> <ru-alpha|ru-bravo|ru-charlie|ru-delta> <b> <F:/warfacebot-master/> (for warface bot)";
 
+            try
+            {
+                Auth.login = args[0];
+            }
+            catch
+            {
+                Console.WriteLine($"Need a login..\n{help_}");
+                Console.ReadKey();
+            }
+
+            try
+            {
+                Auth.password = args[1];
+            }
+            catch
+            {
+                Console.WriteLine($"Need a password..\n{help_}");
+                Console.ReadKey();
+            }
+
+            try
+            {
+                if (args[2] == "ru-alpha")
+                {
+                    Auth.shardid = "0";
+                    Auth.server = "s1.warface.ru";
+                }
+                if (args[2] == "ru-bravo")
+                {
+                    Auth.shardid = "1";
+                    Auth.server = "s2.warface.ru";
+                }
+                if (args[2] == "ru-charlie")
+                {
+                    Auth.shardid = "2";
+                    Auth.server = "s3.warface.ru";
+                }
+                if (args[2] == "ru-delta")
+                {
+                    Auth.shardid = "3";
+                    Auth.server = "s12.warface.ru";
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Need a server..\n{help_}");
+                Console.ReadKey();
+            }
             
-            Auth.login = args[0];
-            Auth.password = args[1];
-            if(args[2] == "ru-alpha")
-            {
-                Auth.shardid = "0";
-                Auth.server = "s1.warface.ru";
-            }
-            if (args[2] == "ru-bravo")
-            {
-                Auth.shardid = "1";
-                Auth.server = "s2.warface.ru";
-            }
-            if (args[2] == "ru-charlie")
-            {
-                Auth.shardid = "2";
-                Auth.server = "s3.warface.ru";
-            }
-            if (args[2] == "ru-delta")
-            {
-                Auth.shardid = "3";
-                Auth.server = "s12.warface.ru";
-            }
             if(args[3] == "g")
             {
                 Start_game = true;
@@ -46,7 +73,15 @@ namespace WarfaceAuth
             {
                 Start_game = false;
             }
-            exe_dir = args[4];
+            try
+            {
+                exe_dir = args[4];
+            }
+            catch
+            {
+                Console.WriteLine($"Need a dir path..\n{help_}");
+                Console.ReadKey();
+            }
 
             Auth.Get_State_Cookies();
         }
